18b1eab35d35a8dbf206793e90413fb86c561176
omac <nidesky@gmail.com>
2015-10-16 00:06:54 +0800

    添加 .gitignore, package.json, gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minify = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+var rename  = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+// gulp.task('sass', function() {
+//     //gulp.src('app/scss/*.scss')
+//     gulp.src('app/scss/main.scss')
+//         .pipe(plumber())
+//         .pipe(sass())
+//         .pipe(autoprefixer({
+//             cascade: false
+//         }))
+//         .pipe(rename('main.css'))
+//         .pipe(minify())
+//         .pipe(gulp.dest('assets/css'))
+//         .pipe(reload({stream: true}));
+// });
+
+// gulp.task('javascript', function() {
+//     gulp.src('app/js/*.js')
+//         .pipe(plumber())
+//         .pipe(concat('all.js'))
+//         .pipe(gulp.dest('assets/js'))
+//         .pipe(reload({stream: true}));
+// });
+
+
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: './151015/'
+        }
+    });
+    // gulp.watch('app/scss/*.scss', ['sass', reload]);
+    // gulp.watch('app/js/*.js', ['javascript', reload]);
+    gulp.watch('**/*.js').on('change', reload);
+    gulp.watch('**/*.css').on('change', reload);
+    gulp.watch('**/*.html').on('change', reload);
+});
+
+gulp.task('default', ['serve']);
