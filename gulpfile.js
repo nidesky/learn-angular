550faccd00f9898586806fc69b970c946b2106d7
omac <nidesky@gmail.com>
2015-10-21 01:20:17 +0800

    directive to controller and directive

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ var reload = browserSync.reload;
 gulp.task('serve', function () {
     browserSync.init({
         server: {
-            baseDir: './151020/'
+            baseDir: './151021/'
         } 
     });
     // gulp.watch('app/scss/*.scss', ['sass', reload]);
